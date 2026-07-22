43743189080220d8035500f0404be61479e98f7e
sspeaks610 <marialith191@gmail.com>
2026-07-22 15:31:33 -0700

    stopgap 18 solution

--- exercises/world-04-deep-signal/ex18-signal-timeout/src/lib.rs
+++ exercises/world-04-deep-signal/ex18-signal-timeout/src/lib.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
-use std::time::Duration;
+use ureq::Error::{Status, Transport};
+use std::{ time::Duration};
+
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct SignalEnvelope {
@@ -21,8 +23,21 @@ pub fn fetch_signal_with_timeout(
     base_url: &str,
     timeout: Duration,
 ) -> Result<SignalEnvelope, SignalTimeoutError> {
-    // ══════════════════════════════════════════════════════════════
-    // 🚀 YOUR MISSION: Replace the todo!() below with your solution.
-    // ══════════════════════════════════════════════════════════════
-    todo!("Configure a timeout-aware GET request and map failures into SignalTimeoutError")
+
+     let res =   ureq::get(base_url)
+        .timeout(timeout)
+        .call()
+        .map_err(|err| {
+            match err {
+                Status(a,_b) => if a == 408 {
+                    SignalTimeoutError::TimedOut
+                } else {
+                    SignalTimeoutError::HttpStatus(a)
+                },
+                Transport(a) => SignalTimeoutError::Transport(a)
+            }
+        })?;
+    let json_raw: String = res.into_string().map_err(SignalTimeoutError::ReadBody)?;
+    let json: SignalEnvelope = serde_json::from_str(&json_raw).map_err(SignalTimeoutError::Decode)?;
+    Ok(json)
 }
